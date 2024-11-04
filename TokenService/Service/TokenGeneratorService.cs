34448ed6e4598853d20542cae4410575da84c941
kahol <kahola@gmail.com>
2024-11-04 13:48:05 +0100

    Created a TokenGeneratorService.cs with hard coded keys and security values.

---
+++ TokenService/Service/TokenGeneratorService.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TokenService.Service;
+
+public class TokenGeneratorService
+{
+    public string GenerateToken(string email)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var secretKey = "92336b63be8446e9a9ea351da752bd1a";  // Load from key vault later
+
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, email),
+            new Claim(ClaimTypes.Role, "Customer")
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            Issuer = "Kimmo Ahola",
+            Audience = "Mille Elfver",
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
