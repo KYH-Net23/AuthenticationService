0696b6f7002c97e930398cf97fa5408a4fbf261e
kahol <kahola@gmail.com>
2024-11-04 13:48:27 +0100

    Created an endpoint for token generation

---
+++ TokenService/Controllers/TokenGeneratorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TokenService.Service;
+
+namespace TokenService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TokenGeneratorController(TokenGeneratorService tokenGeneratorService) : ControllerBase
+{
+    [HttpGet]
+    public string GetToken(string email)
+    {
+        return tokenGeneratorService.GenerateToken(email);
+    }
+}
