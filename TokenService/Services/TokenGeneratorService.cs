f5febbf431825c965122c4f4e4b0d2f7362bc9ae
kahol <kahola@gmail.com>
2024-11-13 22:20:49 +0100

    removed magic numbers

--- TokenService/Services/TokenGeneratorService.cs
+++ TokenService/Services/TokenGeneratorService.cs
@@ -9,7 +9,7 @@ namespace TokenService.Services;
 
 public static class TokenGeneratorService
 {
-    public static string GenerateAccessToken(ResponseContent content, string secretKey)
+    public static string GenerateAccessToken(ResponseContent content, string secretKey, int expirationMinutes)
     {
         var key = Encoding.ASCII.GetBytes(secretKey);
         var claims = new List<Claim>
@@ -17,16 +17,17 @@ public static class TokenGeneratorService
             new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new(JwtRegisteredClaimNames.Sub, content.Id),
             new(JwtRegisteredClaimNames.Email, content.Email),
-            new(JwtRegisteredClaimNames.Name, content.Email),
+            new(JwtRegisteredClaimNames.Name, content.Email)
         };
         claims.AddRange(content.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddMinutes(5),
+            Expires = DateTime.UtcNow.AddMinutes(expirationMinutes),
             Issuer = "https://www.rika.com",
             Audience = "https://www.rika.com",
+            Claims = claims.ToDictionary(claim => claim.Type, object (claim) => claim.Value),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
 
@@ -36,13 +37,11 @@ public static class TokenGeneratorService
     public static string GenerateRefreshToken()
     {
         var randomNumber = new byte[32];
-        using (var rng = RandomNumberGenerator.Create())
-        {
-            rng.GetBytes(randomNumber);
-            return Convert.ToBase64String(randomNumber);
-        }
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
     }
-    public static ClaimsPrincipal GetPrincipalFromExpiredToken(string token, string secretKey)
+    public static ClaimsPrincipal? GetPrincipalFromExpiredToken(string token, string secretKey)
     {
         var key = Encoding.ASCII.GetBytes(secretKey);
         var tokenValidationParameters = new TokenValidationParameters
